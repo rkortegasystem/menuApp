e820a0afbf4d5820fbc84b10a426d2417734af17
RKOrtega94 <44837907+RKOrtega94@users.noreply.github.com>
2018-11-09 15:35:54 -0500

    LoginVeiw

--- app/src/main/java/com/rkosistemas/meseroapp/index.java
+++ app/src/main/java/com/rkosistemas/meseroapp/index.java
@@ -2,11 +2,13 @@ package com.rkosistemas.meseroapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.rkosistemas.meseroapp.connectiondb.ConnectionClass;
 
 public class index extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        ConnectionClass cncl = new ConnectionClass();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
     }
