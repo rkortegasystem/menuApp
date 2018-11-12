55830b171d93d62fa7b501d6f95a716a0be72934
RKOrtega94 <44837907+RKOrtega94@users.noreply.github.com>
2018-11-11 21:24:11 -0500

    Hibernate

--- app/src/main/java/com/rkosistemas/meseroapp/index.java
+++ app/src/main/java/com/rkosistemas/meseroapp/index.java
@@ -2,14 +2,38 @@ package com.rkosistemas.meseroapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import com.rkosistemas.meseroapp.connectiondb.ConnectionClass;
+import android.view.View;
+import android.widget.*;
 
 public class index extends AppCompatActivity {
 
+    TextView txtResult;
+    EditText txtUsername;
+    EditText txtPassword;
+    Button btnLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        ConnectionClass cncl = new ConnectionClass();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
+
+        txtResult = (TextView) findViewById(R.id.txtResult);
+        txtUsername = (EditText) findViewById(R.id.txtUsername);
+        txtPassword = (EditText) findViewById(R.id.txtPassword);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+
+//        btnLogin.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String username = txtUsername.getText().toString();
+//                String password = txtPassword.getText().toString();
+//                LoginController loginController = new LoginController();
+//                if(loginController.verifyUser("160080503", "1600870503")){
+//                    txtResult.setText("Correcto");
+//                } else {
+//                    txtResult.setText("Fallo");
+//                }
+//            }
+//        });
     }
 }
